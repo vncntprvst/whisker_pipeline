52e3b99ff056e1277323d17c1c21f256d6ce7c66
Vincent <prevosto@mit.edu>
2022-12-07 14:41:32 -0500

    Update Whisker_pipeline.m

--- Whisker_pipeline.m
+++ Whisker_pipeline.m
@@ -1,23 +1,36 @@
-function Whisker_pipeline(sessionDir)
+function Whisker_pipeline(sessionDir,whiskParams,cutVids,trackWhisk,collateData)
 % Runs the whisker pipeline.
 % sessionDir: the directory where are the video files
 if nargin == 0; sessionDir = cd; end
+if nargin < 2; whiskParams = true; end
+if nargin < 3; cutVids = true; end
+if nargin < 4; trackWhisk = true; end
+if nargin < 5; collateData = true; end
 
 %% List video files to process
 videoFiles=ListVideoFiles(sessionDir);
 
 %% Generate whisking param file
-[whiskingParams,splitUp]=GetWhiskingParams(sessionDir,videoFiles);
+if whiskParams
+    GetWhiskingParams(sessionDir,videoFiles);
+end
 
 %% Cut videos in 5 second chunks
-CutVideos(sessionDir,videoFiles)
+if cutVids
+    overWrite = false; % overwrite files? 
+    CutVideos(sessionDir,videoFiles,overWrite); 
+end
 
 %% Perform whisker detection
-TrackWhiskers(sessionDir,whiskingParams,splitUp)
+if trackWhisk
+    overWrite = false; % overwrite files? 
+    TrackWhiskers(sessionDir);
+end
 
 %% Concatenate measurements and save whisker data
-SaveWhiskerData(sessionDir);
-
+if collateData
+    SaveWhiskerData(sessionDir);
+end
 
 
 
